de0bd7339d49cbb01dd70914fc1eda113dd15a00
Malakai97 <bhock@umich.edu>
2015-02-04 17:06:23 -0500

    revert previous change

--- db/migrate/20150204220153_rename_supported_protocols.rb
+++
@@ -1,5 +0,0 @@
-class RenameSupportedProtocols < ActiveRecord::Migration
-  def change
-    rename_table :supported_protocols, :nodes_protocols
-  end
-end
--- db/migrate/20150204220237_rename_supported_fixity_algs.rb
+++
@@ -1,5 +0,0 @@
-class RenameSupportedFixityAlgs < ActiveRecord::Migration
-  def change
-    rename_table :supported_fixity_algs, :nodes_fixity_algs
-  end
-end
