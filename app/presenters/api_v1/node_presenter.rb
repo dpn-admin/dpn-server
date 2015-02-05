157036b3d5c0e95ba2f1bbe6ae666fd1e59a229f
Malakai97 <bhock@umich.edu>
2015-02-05 17:23:11 -0500

    Node presenter

--- app/presenters/api_v1/node_presenter.rb
+++ app/presenters/api_v1/node_presenter.rb
@@ -1,34 +1,43 @@
-class ApiV1::NodePresenter
-  def initialize(node)
-    @node = node
-    @hash = nil
-  end
 
-  def to_hash
-    if @hash != nil
+module ApiV1
+  class NodePresenter
+    def initialize(node)
+      @node = node
+      @hash = nil
+    end
+
+    def to_hash
+      if @hash != nil
+        return @hash
+      end
+
+      @hash = {
+        :namespace  => @node.namespace,
+        :name       => @node.name,
+        :ssh_pubkey => @node.ssh_pubkey,
+        :storage => {
+          :region => @node.storage_region.name,
+          :type => @node.storage_type.name
+        },
+        :fixity_algs => @node.fixity_algs.pluck(:name),
+        :protocols => @node.protocols.pluck(:name),
+        :replicate_to => @node.to_nodes.pluck(:namespace),
+        :replicate_from => @node.from_nodes.pluck(:namespace),
+        :created_at => @node.created_at,
+        :updated_at => @node.updated_at
+      }
+
+
+
       return @hash
     end
 
-    @hash = {
-      :namespace  => @node.namespace,
-      :name       => @node.name,
-      :ssh_pubkey => @node.ssh_pubkey,
-      :storage => {
-        :region => StorageRegionPresenter.new(@node.storage_region).to_hash,
-        :type   => StorageTypePresenter.new(@node.storage_type).to_hash
-      },
-      :created_at => @node.created_at,
-      :updated_at => @node.updated_at
-    }
+    def to_json(options = {})
+      return self.to_hash.to_json
+    end
 
+    private
+    attr_reader :node, :hash
 
   end
-
-  def to_json
-    return @hash.to_json
-  end
-
-  private
-  attr_reader :node, :hash
-
 end
