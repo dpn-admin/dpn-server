7de6b989aecc5601630dee524115c7a49561d034
Malakai97 <bhock@umich.edu>
2015-02-06 16:33:42 -0500

    write seeds file that populates initial database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+[:requested, :rejected, :received, :confirmed, :cancelled].each do |status|
+  ReplicationStatus.create(:name => status)
+end
+
+[:requested, :accepted, :rejected, :prepared, :finished, :cancelled].each do |status|
+  RestoreStatus.create(:name => status)
+end
+
+sha256 = FixityAlg.create(:name => :sha256)
+md5 = FixityAlg.create(:name => :md5)
+
+rsync = Protocol.create(:name => :rsync)
+
+default_storage_region = StorageRegion.create(:name => :default)
+default_storage_type = StorageType.create(:name => :default)
+
+node_objects = []
+nodes = [:test, :sdr, :tdr, :hathi, :aptrust, :chron]
+nodes.each do |namespace|
+  node_objects << Node.create(:namespace => namespace, :name => namespace) do |node|
+    node.storage_region = default_storage_region
+    node.storage_type = default_storage_type
+    node.fixity_algs << md5
+    node.fixity_algs << sha256
+    node.protocols << rsync
+  end
+end
+
+node_objects.each do |from_node|
+  node_objects.each do |to_node|
+    if from_node != to_node
+      from_node.to_nodes << to_node
+    end
+  end
+end
+
