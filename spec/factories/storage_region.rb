6460ee8792823f2e633fa9282bb76b57b9a59702
Malakai97 <bhock@umich.edu>
2015-03-04 15:50:18 -0500

    tests for storage_region and changes needed to pass them

---
+++ spec/factories/storage_region.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :storage_region do |f|
+    f.name "someregion"
+  end
+end
---
+++ spec/models/storage_region_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe StorageRegion do
+  it "has a valid factory" do
+    expect(create(:storage_region)).to be_valid
+  end
+
+  it "is invalid without a name" do
+    expect {
+      create(:storage_region, name: nil)
+    }.to raise_error
+  end
+
+  it "can find records" do
+    name = "herp"
+    create(:storage_region, name: name)
+
+    instance = StorageRegion.where(name: name).first
+
+    expect(instance).to be_valid
+  end
+
+  it "should store name as lowercase" do
+    name  = "aSDFdsfadsSDFsd"
+    create(:storage_region, name: name)
+
+    instance = StorageRegion.where(name: name.downcase).first
+
+    expect(instance).to be_valid
+    expect(instance.name).to eql(name.downcase)
+  end
+end
