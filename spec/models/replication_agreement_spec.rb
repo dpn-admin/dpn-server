bd1d3576d0b2f8b965edc6f04042fc1a82f4d813
Malakai97 <bhock@umich.edu>
2015-03-06 13:53:20 -0500

    replication agreement tests passing

--- spec/models/replication_agreement_spec.rb
+++ spec/models/replication_agreement_spec.rb
@@ -5,13 +5,26 @@ describe ReplicationAgreement do
     expect(Fabricate(:replication_agreement)).to be_valid
   end
 
-  it "has a valid factory if I handhold it" do
-    from_node = Fabricate(:node, namespace: "some_from_node")
-    to_node = Fabricate(:node, namespace: "some_to_node")
-
-    ra = Fabricate(:replication_agreement, from_node: from_node, to_node: to_node)
-
-    expect(ra).to be_valid
-
+  it "has a valid from_node" do
+    from_node = Fabricate.build(:replication_agreement).from_node
+    expect(from_node).to be_valid
   end
+
+  it "has a valid to_node" do
+    to_node = Fabricate.build(:replication_agreement).to_node
+    expect(to_node).to be_valid
+  end
+
+  it "is invalid without a from_node" do
+    expect {
+      Fabricate(:replication_agreement, from_node: nil)
+    }.to raise_error(ActiveRecord::StatementInvalid)
+  end
+
+  it "is invalid without a to_node" do
+    expect {
+      Fabricate(:replication_agreement, to_node: nil)
+    }.to raise_error(ActiveRecord::StatementInvalid)
+  end
+
 end
