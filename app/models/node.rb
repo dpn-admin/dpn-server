008350a6c6ccb94e27b44a71c94cb5544c525e5a
Bryan Hockey <bhock@umich.edu>
2015-03-18 14:56:49 -0400

    bag model now references ingest node instead of original node

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,5 +1,5 @@
 class Node < ActiveRecord::Base
-  has_many :original_bags, :class_name => "Bag", :foreign_key => "original_node_id"
+  has_many :ingest_bags, :class_name => "Bag", :foreign_key => "ingest_node_id"
   has_many :admin_bags, :class_name => "Bag", :foreign_key => "admin_node_id"
 
   belongs_to :storage_region
