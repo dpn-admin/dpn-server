99e297484e30074e48e1563ee50c18d3d0ec2f7a
Bryan Hockey <bhock@umich.edu>
2015-03-18 12:28:27 -0400

    lowercased concern now handles nils correctly

--- app/models/concerns/lowercased.rb
+++ app/models/concerns/lowercased.rb
@@ -6,16 +6,24 @@ module Lowercased
 
       # Override the field=(value) method.
       define_method("#{field}=") do |value|
-        write_attribute(field.to_sym, value.downcase)
+        if value.nil?
+          write_attribute(field.to_sym, value)
+        else
+          write_attribute(field.to_sym, value.downcase)
+        end
       end
 
       # Override the find_by_field(value) class method.
       define_singleton_method("find_by_#{field}") do |value|
-        super(value.downcase)
+        if value.nil?
+          super(value)
+        else
+          super(value.downcase)
+        end
       end
 
       # Create a validation
-      validates field.to_sym, format: { with: /[^A-Z\s]+/, message: "Whitespace and capitals not allowed"}
+      validates field.to_sym, format: { with: /[^A-Z\s]+/, message: "does not allow whitespace or capital letters."}
 
     end
   end
