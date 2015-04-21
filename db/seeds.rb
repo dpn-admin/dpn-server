c571a0d71325b8be1bc3335fdfd0b24caf7fb681
Bryan Hockey <bhock@umich.edu>
2015-04-21 14:57:03 -0400

    better seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,40 +6,60 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-[:requested, :rejected, :received, :confirmed, :stored, :cancelled].each do |status|
-  ReplicationStatus.create(:name => status)
+require "securerandom"
+require "json"
+
+if Rails.env.production?
+  hash = JSON.parse(File.read("db/seeds/prod.seeds.json"), symbolize_names: true)
+else
+  hash = JSON.parse(File.read("db/seeds/dev.seeds.json"), symbolize_names: true)
 end
 
-[:requested, :accepted, :rejected, :prepared, :finished, :cancelled].each do |status|
-  RestoreStatus.create(:name => status)
+hash[:replication_status].each do |status|
+  ReplicationStatus.create!(name: status)
 end
 
-sha256 = FixityAlg.create(:name => :sha256)
-md5 = FixityAlg.create(:name => :md5)
-
-rsync = Protocol.create(:name => :rsync)
-
-default_storage_region = StorageRegion.create(:name => :default)
-default_storage_type = StorageType.create(:name => :default)
-
-node_objects = []
-nodes = [:sdr, :tdr, :hathi, :aptrust, :chron]
-nodes.each do |namespace|
-  node_objects << Node.create(:namespace => namespace, :name => namespace) do |node|
-    node.storage_region = default_storage_region
-    node.storage_type = default_storage_type
-    node.fixity_algs << md5
-    node.fixity_algs << sha256
-    node.protocols << rsync
-  end
+hash[:restore_status].each do |status|
+  RestoreStatus.create!(name: status)
 end
 
-node_objects.each do |from_node|
-  node_objects.each do |to_node|
+hash[:fixity_alg].each do |alg|
+  FixityAlg.create!(name: alg)
+end
+
+hash[:protocol].each do |protocol|
+  Protocol.create!(name: protocol)
+end
+
+hash[:storage_region].each do |region|
+  StorageRegion.create!(name: region)
+end
+
+hash[:storage_type].each do |type|
+  StorageType.create!(name: type)
+end
+
+nodes = []
+hash[:node].each_value do |node_entry|
+  new_node = Node.create!(
+      namespace: node_entry[:namespace],
+      name: node_entry[:namespace],
+      storage_region: StorageRegion.find_by_name(node_entry[:storage_region]),
+      storage_type: StorageType.find_by_name(node_entry[:storage_type]),
+      fixity_algs: FixityAlg.where(name: node_entry[:fixity_algs]),
+      protocols: Protocol.where(name: node_entry[:protocols]),
+      api_root: node_entry[:api_root],
+      private_auth_token: node_entry[:private_auth_token])
+  nodes << new_node
+end
+
+nodes.each do |from_node|
+  nodes.each do |to_node|
     if from_node != to_node
       from_node.replicate_to_nodes << to_node
       from_node.restore_to_nodes << to_node
     end
+    from_node.save!
   end
 end
 
