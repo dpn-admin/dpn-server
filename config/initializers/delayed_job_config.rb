e11f4748c52de0a8aed51d71bdcf742455c8cde0
Bryan Hockey <bhock@umich.edu>
2015-06-11 17:51:57 -0400

    enable delayed_job logging

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -6,4 +6,4 @@ Delayed::Worker.max_run_time = 48.hours
 Delayed::Worker.default_queue_name = 'default'
 Delayed::Worker.delay_jobs = !Rails.env.test?
 #Delayed::Worker.raise_signal_exceptions = :term
-#Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
+Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
