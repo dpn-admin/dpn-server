17f3008f9d7ce7a17f874125bc07a6761f3b8e3c
Michael Ritter <shake@umiacs.umd.edu>
2015-09-01 17:14:42 -0400

    Update fabricators to include a member

---
+++ spec/fabricators/member_fabricator.rb
@@ -0,0 +1,9 @@
+# Copyright (c) 2015 The Regents of the University of Michigan.
+# All Rights Reserved.
+# Licensed according to the terms of the Revised BSD License
+# See LICENSE.md for details.
+
+Fabricator(:member) do
+  uuid { SecureRandom.uuid }
+  name { Faker::Company.name }
+end
