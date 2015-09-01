1ed669a06d5920212189766ba9d17cafdf1ff373
Michael Ritter <shake@umiacs.umd.edu>
2015-09-01 14:00:04 -0400

    Fix typos in member migrations

--- db/migrate/20150831175103_create_members.rb
+++ db/migrate/20150831175103_create_members.rb
@@ -1,9 +1,15 @@
+
+
+
+
+
+
 class CreateMembers < ActiveRecord::Migration
   def change
     create_table :members do |t|
-        t.string :uuid
-        t.string :member_name
+      t.string :uuid
+      t.string :name
     end
-    add_index :members, :uuid, :unique true
+    add_index :members, :uuid, unique: true
   end
 end
--- db/migrate/20150831175410_add_members_foreign_keys.rb
+++ db/migrate/20150831175410_add_members_foreign_keys.rb
@@ -4,8 +4,8 @@ class AddMembersForeignKeys < ActiveRecord::Migration
       add_column :bags, :member_id, :string
       
       add_foreign_key :bags, :members,
-          :column: :member_id,
-          :on_delete: :restrict,
-          :on_update: :cascade
+          column: :member_id,
+          on_delete: :restrict,
+          on_update: :cascade
   end
 end
