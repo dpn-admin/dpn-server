85b92c61b2831a2765bcdd24a7a786657f370064
Michael Ritter <shake@umiacs.umd.edu>
2015-09-01 17:13:05 -0400

    Add email to member migration

--- db/migrate/20150831175103_create_members.rb
+++ db/migrate/20150831175103_create_members.rb
@@ -1,14 +1,11 @@
-
-
-
-
-
+# Yo license information
 
 class CreateMembers < ActiveRecord::Migration
   def change
     create_table :members do |t|
       t.string :uuid
       t.string :name
+      t.string :email
     end
     add_index :members, :uuid, unique: true
   end
