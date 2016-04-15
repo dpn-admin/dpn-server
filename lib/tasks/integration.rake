a1d7df41f25918e924cf8e72445e953b01b5faa3
A. Diamond <diamond@sheprador.com>
2016-04-15 13:50:20 -0400

    Moved code from rake to helper class

--- lib/tasks/integration.rake
+++ lib/tasks/integration.rake
@@ -8,22 +8,18 @@
 # other demo nodes.
 #
 
-namespace :integration do
+require_relative '../../test/helpers/fixture_helper.rb'
 
-  TEST_MEMBER_UUID = "471fe920-bea3-4eb3-aafc-7841f8160479"
+namespace :integration do
 
   desc "Delete data used in internode testing, but preserve nodes and members"
   task delete_test_data: :environment do
     if Rails.env == "production"
       raise "Deleting data is not allowed in the production environment"
     end
-    puts "Deleting all RestoreTransfers, ReplicationTransfers, " +
-         "FixityChecks, Bags and VersionFamilies"
-    RestoreTransfer.delete_all
-    ReplicationTransfer.delete_all
-    FixityCheck.delete_all
-    Bag.destroy_all
-    VersionFamily.destroy_all
+    puts "Deleting all Bags and Transfers"
+    fixture_helper = FixtureHelper.new()
+    fixture_helper.delete_fixtures
   end
 
   desc "Load fixture bags and replication requests for testing"
@@ -31,139 +27,18 @@ namespace :integration do
     if Rails.env == "production"
       raise "You may not load test fixtures in the production environment"
     end
-
-    local_namespace = Rails.application.config.local_namespace
-    fixture_dir = "#{Rails.root}/test/fixtures/integration/#{local_namespace}"
-
-    puts "Reloading RestoreTransfers, ReplicationTransfers, " +
-         "FixityChecks, Bags and VersionFamilies from #{fixture_dir}"
-
-    versions = load_yaml_file(File.join(fixture_dir, 'version_families.yml'))
-    bags = load_yaml_file(File.join(fixture_dir, 'bags.yml'))
-    fixities = load_yaml_file(File.join(fixture_dir, 'fixity_checks.yml'))
-    replications = load_yaml_file(File.join(fixture_dir, 'replication_transfers.yml'))
-    restores = load_yaml_file(File.join(fixture_dir, 'restore_transfers.yml'))
-
-    # Create VersionFamily records
-    puts "Creating VersionFamily records"
-    versions.values.each do |v|
-      VersionFamily.create(uuid: v['uuid'])
-    end
-
-    # Create Bag records
-    # Use capital T in Test for Postgres
-    test_member = get_test_member
-    this_node = Node.where(namespace: local_namespace).first
-    if this_node.nil?
-      raise "Cannot create bags because system has no Node record for '#{local_namespace}'."
-    end
-    now = Time.now.utc
-    puts "Creating bags"
-    bags.values.each do |b|
-      version_family = VersionFamily.where(uuid: b['uuid']).first
-      Bag.create(uuid: b['uuid'],
-                 ingest_node: this_node,
-                 admin_node: this_node,
-                 local_id: b['local_id'],
-                 size: b.size,
-                 version: b['version'],
-                 type: "DataBag",
-                 created_at: now,
-                 updated_at: now,
-                 version_family: version_family,
-                 member: test_member)
-    end
-
-    # Create FixityCheck records
-    puts 'Creating FixityChecks'
-    sha256 = FixityAlg.where(name: 'sha256').first
-    if sha256.nil?
-      raise "Cannot load FixityChecks because FixityAlg sha256 is missing"
-    end
-    fixities.values.each do |f|
-      # Get the bag uuid from the fixture data
-      this_bag = bags[f['bag']]
-      bag_uuid = this_bag['uuid']
-      # Now look up the actual bag record in the DB
-      bag = Bag.where(uuid: bag_uuid).first
-      FixityCheck.create(bag: bag, fixity_alg: sha256, value: f['value'])
-    end
-
-    # Create ReplicationTransfer records
-    rsync = Protocol.where(name: 'rsync').first
-    if rsync.nil?
-      raise "Cannot create Replication: 'rsync' is not in protocols table"
-    end
-    replications.values.each do |r|
-      to_node = Node.where(namespace: r['to_node']).first
-      if to_node.nil?
-        raise "Cannot create Repication: to_node '#{r['to_node']}' is missing from the node table."
-      end
-      this_bag = bags[r['bag']]
-      bag_uuid = this_bag['uuid']
-      bag = Bag.where(uuid: bag_uuid).first
-      ReplicationTransfer.create(link: r['link'],
-                                 bag_valid: true,
-                                 fixity_accept: false,
-                                 created_at: now,
-                                 updated_at: now,
-                                 replication_id: r['replication_id'],
-                                 bag: bag,
-                                 from_node: this_node,
-                                 to_node: to_node,
-                                 status: 0,
-                                 protocol: rsync,
-                                 fixity_alg: sha256)
-    end
-
-    # Create ReplicationTransfer records
-    restores.values.each do |r|
-      from_node = Node.where(namespace: r['from_node']).first
-      this_bag = bags[r['bag']]
-      bag_uuid = this_bag['uuid']
-      bag = Bag.where(uuid: bag_uuid).first
-      RestoreTransfer.create(link: r['link'],
-                             created_at: now,
-                             updated_at: now,
-                             restore_id: r['restore_id'],
-                             bag: bag,
-                             from_node: from_node,
-                             to_node: this_node,
-                             status: 0,
-                             protocol: rsync)
-    end
+    fixture_helper = FixtureHelper.new()
+    puts "Loading Bags and Transfers from #{fixture_helper.fixture_dir}"
+    fixture_helper.load_fixtures
   end
 
   desc "Delete internode testing data and reload from fixtures"
   task reset: :environment do
-    if Rails.env == "production"
-      raise "Test data reset is not allowed in the production environment"
-    end
     Rake::Task["integration:delete_test_data"].invoke
     Rake::Task["integration:load_fixtures"].invoke
-
     if Rails.env == "demo" && Rails.application.config.local_namespace == "aptrust"
       set_links_for_aptrust
     end
-  end
-
-  # Loads the specified YAML file, replacing embedded variables
-  # like <%= Rails.root %> with their proper values.
-  def load_yaml_file(filepath)
-    YAML.load(ERB.new(File.read(filepath)).result)
-  end
-
-  def get_test_member
-    test_member = Member.where(uuid: TEST_MEMBER_UUID).first
-    if test_member.nil?
-      now = Time.now.utc
-      test_member = Member.create(uuid: TEST_MEMBER_UUID,
-                                  name: "Integration Test University",
-                                  email: "integration_test@example.com",
-                                  created_at: now,
-                                  updated_at: now)
-    end
-    test_member
   end
 
   # The replication transfer links in the fixture files work
---
+++ test/helpers/fixture_helper.rb
@@ -0,0 +1,232 @@
+#
+# FixtureHelper loads fixtures for integration tests. Use this in
+# your local environment and on your demo server to seed or reset
+# your database for integration testing.
+#
+# Use this instead of ActiveRecord::FixtureSet on the demo/staging
+# servers. ActiveRecord::FixtureSet generates record ids by hashing
+# the record labels from the YAML fixture files. Those generated ids
+# will never correspond to the actual records in the tables for
+# Node, Protocol, FixityAlg and Member. That means fixtures will not
+# load at all on demo/staging servers using ActiveRecord::FixtureSet.
+#
+class FixtureHelper
+
+  TEST_MEMBER_UUID = "471fe920-bea3-4eb3-aafc-7841f8160479"
+
+  def initialize
+    @local_namespace = Rails.application.config.local_namespace
+    @fixture_dir = "#{Rails.root}/test/fixtures/integration/#{@local_namespace}"
+  end
+
+  # This delete ALL of the VersionFamily, Bag, FixityCheck, ReplicationTransfer
+  # and RestoreTransfer records from the database. This will raise an exception
+  # if you try to run it in production.
+  #
+  # This does NOT delete core information that the system needs to run, such
+  # as Node, Member, FixityAlg or Protocol records.
+  def delete_fixtures
+    raise "No deleting data in production!" unless safe_environment?
+    RestoreTransfer.delete_all
+    ReplicationTransfer.delete_all
+    FixityCheck.delete_all
+    Bag.destroy_all
+    VersionFamily.destroy_all
+  end
+
+  # This loads fixtures from the following files into the database:
+  #
+  # test/fixtures/integration/<namespace>/version_families.yml
+  # test/fixtures/integration/<namespace>/bags.yml
+  # test/fixtures/integration/<namespace>/fixity_checks.yml
+  # test/fixtures/integration/<namespace>/replication_transfers.yml
+  # test/fixtures/integration/<namespace>/restore_transfers.yml
+  #
+  # This will raise an exception if you try to run it in production.
+  def load_fixtures
+    raise "No loading fixtures in production!" unless safe_environment?
+
+    # Set vars we'll need while loading...
+    @this_node = Node.where(namespace: @local_namespace).first
+    if @this_node.nil?
+      raise "Cannot load fixtures because system has no Node record for '#{@local_namespace}'."
+    end
+
+    # Load data from YAML fixture files.
+    @versions = load_yaml_file(File.join(@fixture_dir, 'version_families.yml'))
+    @bags = load_yaml_file(File.join(@fixture_dir, 'bags.yml'))
+    @fixities = load_yaml_file(File.join(@fixture_dir, 'fixity_checks.yml'))
+    @replications = load_yaml_file(File.join(@fixture_dir, 'replication_transfers.yml'))
+    @restores = load_yaml_file(File.join(@fixture_dir, 'restore_transfers.yml'))
+
+    # Now push the data in the database
+    load_version_families
+    load_bags
+    load_fixity_checks
+    load_replication_transfers
+    load_restore_transfers
+  end
+
+  # Loads the specified YAML file, replacing embedded variables
+  # like <%= Rails.root %> with their proper values. If the items
+  # in the YAML file have labels, this will return a hash of objects
+  # in which the keys are the labels and the values are the objects.
+  # Otherwise, it will return an array of objects.
+  def load_yaml_file(filepath)
+    yaml_data = YAML.load(ERB.new(File.read(filepath)).result)
+    if yaml_data.is_a?(Hash)
+      return_data = {}
+      yaml_data.each do |key, value|
+        return_data[key] = OpenStruct.new(value)
+      end
+    else
+      return_data = []
+      yaml_data.each do |item|
+        return_data.push(OpenStruct.new(item))
+      end
+    end
+    return_data
+  end
+
+  # Returns true if we're NOT running in the production environment.
+  # We don't want to be loading fixtures or erasing anything in the
+  # production environment.
+  def safe_environment?
+    Rails.env != "production"
+  end
+
+  # Returns the name of the directory from which fixtures are loaded.
+  def fixture_dir
+    @fixture_dir
+  end
+
+  # Returns the Protocol object for the rsync protocol.
+  # Creates it if it doesn't exist.
+  def rsync
+    @rsync ||= Protocol.where(name: 'rsync').first
+    if @rsync.nil?
+      now = Time.now.utc
+      @rsync = Protocol.create(name: 'rsync', created_at: now, updated_at: now)
+    end
+    @rsync
+  end
+
+  # Returns the FixityAlg object for the sha256 algorithm.
+  # Creates it if it does not exist.
+  def sha256
+    @sha256 ||= FixityAlg.where(name: 'sha256').first
+    if @sha256.nil?
+      now = Time.now.utc
+      @sha256 = FixityAlg.create(name: 'sha256', created_at: now, updated_at: now)
+    end
+    @sha256
+  end
+
+  # Returns a test member who will be the owner of all of the
+  # bags used in integration test. If this member does not exist,
+  # this method will create it.
+  def test_member
+    @test_member ||= Member.where(uuid: TEST_MEMBER_UUID).first
+    if @test_member.nil?
+      now = Time.now.utc
+      test_member = Member.create(uuid: TEST_MEMBER_UUID,
+                                  name: "Integration Test University",
+                                  email: "integration_test@example.com",
+                                  created_at: now,
+                                  updated_at: now)
+    end
+    @test_member
+  end
+
+  private
+
+  # Loads VersionFamily records from
+  # test/fixtures/integration/<namespace>/version_families.yml
+  # into the database.
+  def load_version_families
+    @versions.values.each do |v|
+      VersionFamily.create(uuid: v.uuid)
+    end
+  end
+
+  # Loads Bag records from
+  # test/fixtures/integration/<namespace>/bags.yml
+  # into the database. Requires version_families be
+  # loaded first.
+  def load_bags
+    now = Time.now.utc
+    @bags.values.each do |bag|
+      version_family = VersionFamily.where(uuid: bag.uuid).first
+      Bag.create!(uuid: bag.uuid,
+                  ingest_node: @this_node,
+                  admin_node: @this_node,
+                  local_id: bag.local_id,
+                  size: bag.size,
+                  version: bag.version,
+                  type: "DataBag",
+                  created_at: now,
+                  updated_at: now,
+                  version_family: version_family,
+                  member: self.test_member)
+    end
+  end
+
+  # Loads FixityCheck records from
+  # test/fixtures/integration/<namespace>/fixity_checks.yml
+  # into the database. Requires bags be loaded first.
+  def load_fixity_checks
+    @fixities.values.each do |fixity|
+      bag_uuid = @bags[fixity.bag].uuid
+      bag = Bag.where(uuid: bag_uuid).first
+      FixityCheck.create!(bag: bag, fixity_alg: self.sha256, value: fixity.value)
+    end
+  end
+
+  # Loads ReplicationTransfer records from
+  # test/fixtures/integration/<namespace>/replication_transfers.yml
+  # into the database. Requires bags be loaded first.
+  def load_replication_transfers
+    now = Time.now.utc
+    @replications.values.each do |xfer|
+      to_node = Node.where(namespace: xfer.to_node).first
+      raise "to_node '#{xfer.to_node}' is missing from the node table." if to_node.nil?
+      bag_uuid = @bags[xfer.bag].uuid
+      bag = Bag.where(uuid: bag_uuid).first
+      ReplicationTransfer.create(link: xfer.link,
+                                 bag_valid: true,
+                                 fixity_accept: false,
+                                 created_at: now,
+                                 updated_at: now,
+                                 replication_id: xfer.replication_id,
+                                 bag: bag,
+                                 from_node: @this_node,
+                                 to_node: to_node,
+                                 status: 0,
+                                 protocol: self.rsync,
+                                 fixity_alg: self.sha256)
+    end
+  end
+
+  # Loads RestoreTransfer records from
+  # test/fixtures/integration/<namespace>/restore_transfers.yml
+  # into the database. Requires bags be loaded first.
+  def load_restore_transfers
+    now = Time.now.utc
+    @restores.values.each do |xfer|
+      from_node = Node.where(namespace: xfer.from_node).first
+      raise "from_node '#{xfer.from_node}' is missing from the node table." if from_node.nil?
+      bag_uuid = @bags[xfer.bag].uuid
+      bag = Bag.where(uuid: bag_uuid).first
+      RestoreTransfer.create(link: xfer.link,
+                             created_at: now,
+                             updated_at: now,
+                             restore_id: xfer.restore_id,
+                             bag: bag,
+                             from_node: from_node,
+                             to_node: @this_node,
+                             status: 0,
+                             protocol: self.rsync)
+    end
+  end
+
+end
