db964007b14e0cb1f28cf554d8c95e750e318b0f
Bryan Hockey <bhock@umich.edu>
2016-05-02 17:55:30 -0400

    message digest model passing tests

--- app/models/message_digest.rb
+++ app/models/message_digest.rb
@@ -8,4 +8,21 @@ class MessageDigest < ActiveRecord::Base
   belongs_to :node
   belongs_to :bag
   belongs_to :fixity_alg
+
+  ### ActiveModel::Dirty Validations
+  validates :bag_id,        read_only: true, on: :update
+  validates :fixity_alg_id, read_only: true, on: :update
+  validates :value,         read_only: true, on: :update
+  validates :node_id,       read_only: true, on: :update
+  validates :created_at,    read_only: true, on: :update
+
+  ### Static Validations
+  validates :bag,         presence: true
+  validates :fixity_alg,  presence: true
+  validates :value,       presence: true
+  validates :node,        presence: true
+  validates :created_at,  presence: true
+  validates_uniqueness_of :bag, scope: :fixity_alg
+
+
 end
--- spec/models/message_digest_spec.rb
+++ spec/models/message_digest_spec.rb
@@ -11,7 +11,7 @@ describe Digest do
     expect(Fabricate(:message_digest)).to be_valid
   end
 
-  [:bag, :node, :fixity_alg, :value, :created_at].each do |field|
+  [:bag, :node, :fixity_alg, :value].each do |field|
     it "is invalid without a #{field}" do
       expect(Fabricate.build(:message_digest, field => nil)).to_not be_valid
     end
@@ -21,6 +21,15 @@ describe Digest do
       other_record = Fabricate(:message_digest)
       expect(record.update(field => other_record.send(field))).to be false
     end
+  end
+
+  it "is invalid without created_at" do
+    expect(Fabricate.build(:message_digest, created_at: nil)).to_not be_valid
+  end
+
+  it "disallows changing created_at" do
+    record = Fabricate(:message_digest)
+    expect(record.update(created_at: 2.seconds.from_now)).to be false
   end
 
   it "is unique by fixity_alg-bag pairs" do
