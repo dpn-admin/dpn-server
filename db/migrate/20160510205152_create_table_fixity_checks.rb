a157de58a322a5a66855e5781ed3c7bb728a56ae
Bryan Hockey <bhock@umich.edu>
2016-05-10 17:27:16 -0400

    add copyright statement to migration

--- db/migrate/20160510205152_create_table_fixity_checks.rb
+++ db/migrate/20160510205152_create_table_fixity_checks.rb
@@ -1,3 +1,8 @@
+# Copyright (c) 2015 The Regents of the University of Michigan.
+# All Rights Reserved.
+# Licensed according to the terms of the Revised BSD License
+# See LICENSE.md for details.
+
 class CreateTableFixityChecks < ActiveRecord::Migration
   def change
     create_table :fixity_checks do |t|
