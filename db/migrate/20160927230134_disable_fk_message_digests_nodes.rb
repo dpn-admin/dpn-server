9d6ffbf7e1f8259f6a78b396049555063894eb08
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2016-10-07 14:27:34 -0700

    fu Fix message digests migrations

--- db/migrate/20160927230134_disable_fk_message_digests_nodes.rb
+++ db/migrate/20160927230134_disable_fk_message_digests_nodes.rb
@@ -9,9 +9,16 @@ class DisableFkMessageDigestsNodes < ActiveRecord::Migration
 
   def up
     remove_foreign_key :message_digests, :nodes
+    remove_column :message_digests, :node_id
   end
 
   def down
+    add_column :message_digests, :node_id, :integer
+
+    MessageDigest.all.each do |md|
+      md.update!(node_id: md.bag.admin_node_id)
+    end
+
     add_foreign_key :message_digests, :nodes,
       name: 'message_digests_nodes_id_fk',
       column: 'node_id',
--- db/migrate/20160927230135_node_not_null_in_message_digest.rb
+++ db/migrate/20160927230135_node_not_null_in_message_digest.rb
@@ -1,12 +1,13 @@
 class NodeNotNullInMessageDigest < ActiveRecord::Migration
   def up
-    MessageDigest.where(node_id: nil).all.each do |md|
+    add_column :message_digests, :node_id, :integer, null: false
+
+    MessageDigest.all.each do |md|
       md.update!(node_id: md.bag.admin_node_id)
     end
-    change_column :message_digests, :node_id, :integer, null: false
   end
 
   def down
-    change_column :message_digests, :node_id, :integer, null: true
+    remove_column :message_digests, :node_id
   end
 end
